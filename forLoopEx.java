a99d95886ec4defeb58f09d0228cd1383c7edc5a
prakashraj2811 <157130400+prakashraj2811@users.noreply.github.com>
2024-01-19 19:55:25 +0530

    Create forLoopEx.java

---
+++ forLoopEx.java
@@ -0,0 +1,22 @@
+package class1;
+import java.util.Scanner;
+public class oddore
+{
+	public static void main(String args[])
+	{
+		int i=0;
+		for ( i=0;i<30;i++)
+		{
+			if (i%2==0)
+			{
+				System.out.println("it is even"+i);
+				
+			}
+			else
+			{
+				System.out.println("it is odd"+i);
+			}
+		}
+		
+	}
+}
